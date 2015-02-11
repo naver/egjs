347a9b9ef473e62a0bb7a7ff3d172a8df7bd2668
sculove <sculove@gmail.com>
2015-02-11 21:35:42 +0900

    naver 네임스페이스 추가, agent 노출 제거, useCss3d 명 변경 useCss3d 내용 정리, jQuery HTML 코딩 컨벤션 적용

--- src/extend.js
+++ src/extend.js
@@ -3,12 +3,8 @@
 		_patch = {},
 		_check = function(method) {
 			var v = null;
-			if(_patch[method] != undefined) {
-				if(typeof _patch[method] == "function") {
-					v = _patch[method]();
-				} else {
-					v = Number(_patch[method]);
-				}
+			if(typeof _patch[method] == "function") {
+				v = _patch[method](_agent);
 				switch (v){
 					case -1 : return false;
 					case 1 : return true;
@@ -19,14 +15,14 @@
 		},
 		_agent = (function() {
 			var osMatch = /(iPhone|iPad) OS ([\w.]+)/.exec(_ua) ||
-				/(Android) ([\w.]+)/.exec(_ua) ||
-				[];
+					/(Android) ([\w.]+)/.exec(_ua) ||
+					[],
+				browserMatch = /(Chrome|CriOS)[ \/]([\w.]+)/.exec(_ua) ||
+					[];
 			if(osMatch.length >= 2) {
 				osMatch[1] = /iPhone|iPad/.test(osMatch[1]) ? "ios" : osMatch[1].toLowerCase() ;
 				osMatch[2] = osMatch[2].replace(/\_/g,'.').replace(/\s/g, "");
 			}
-			var browserMatch = /(Chrome|CriOS)[ \/]([\w.]+)/.exec(_ua) ||
-				[];
 			if(browserMatch.length >= 2) {
 				if(/Chrome|CriOS/.test(browserMatch[1])) {
 					browserMatch[1] = /SAMSUNG/.test(_ua) ? "sbrowser" : "chrome";
@@ -41,43 +37,36 @@
 		})();
 
 	$.extend({
-		agent : _agent,
-		default : _patch,
-		useCss3d : function() {
-			var check = _check("useCss3d");
-			if(check != null) {
-				return check;
-			}
-
-			// chrome (less then 25) has a text blur bug.
-			// but samsung sbrowser fix it.
-			if(/chrome/.test(_agent.browser) && _agent.browserVersion < "25") {
-				return false;
-			}
-
-			var result = false;
-			if(/ios/.test(_agent.os)) {
-				return true;
-			} else if(/android/.test(_agent.os)) {
-				var ua = _ua.match(/\(.*\)/);
-				if(ua instanceof Array && ua.length > 0){
-					ua=ua[0];
-				}
-				if(_agent.osVersion >= "4.1.0") {
-					// android 4.1+ blacklist
-					if(!/EK-GN120|SM-G386F/.test(ua)) {	// EK-GN120 : Galaxy Camera, SM-G386F : Galaxy Core LTE
-						result = true;
-					}
+		naver : {
+			defaults : _patch,
+			isHardwareAccelerable : function() {
+				var check = _check("isHardwareAccelerable");
+				if(check != null) {
+					return check;
 				} else {
-					if(_agent.osVersion >= "4.0.3" &&
-						/SHW-|SHV-|GT-|SCH-|SGH-|SPH-|LG-F160|LG-F100|LG-F180|LG-F200|EK-|IM-A|LG-F240|LG-F260/.test(ua) &&
-						!/SHW-M420|SHW-M200|GT-S7562/.test(ua)) {    // SHW-M420 : Galaxy Nexus , SHW-M200 : NexusS , GT-S7562 : Galaxy S duos
+					var result = false;
+					// chrome (less then 25) has a text blur bug.
+					// but samsung sbrowser fix it.
+					if(/chrome/.test(_agent.browser) && _agent.browserVersion < "25") {
+						result = false;
+					} else if(/android/.test(_agent.os)) {
+						var ua = _ua.match(/\(.*\)/);
+						if(ua instanceof Array && ua.length > 0){
+							ua=ua[0];
+						}
+						// android 4.1+ blacklist
+						// EK-GN120 : Galaxy Camera, SM-G386F : Galaxy Core LTE
+						result = (_agent.osVersion >= "4.1.0" && !/EK-GN120|SM-G386F/.test(ua)) ||
+							// SHW-M420 : Galaxy Nexus , SHW-M200 : NexusS , GT-S7562 : Galaxy S duos
+							(_agent.osVersion >= "4.0.3" &&
+								/SHW-|SHV-|GT-|SCH-|SGH-|SPH-|LG-F160|LG-F100|LG-F180|LG-F200|EK-|IM-A|LG-F240|LG-F260/.test(ua) &&
+								!/SHW-M420|SHW-M200|GT-S7562/.test(ua));
+					} else if(/ios/.test(_agent.os)) {
 						result = true;
 					}
+					return result;
 				}
-
 			}
-			return result;
 		}
 	});
 })(jQuery);
