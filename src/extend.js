4f2bd173616a189d3e8796659c2c8c4df3ce249d
sculove <sculove@gmail.com>
2015-03-16 11:33:58 +0900

    chnage code convension

--- src/extend.js
+++ src/extend.js
@@ -1,10 +1,10 @@
 (function($) {
-	var _ua = navigator.userAgent,
-		_patch = {},
-		_check = function(method) {
+	var ua = navigator.userAgent,
+		patch = {},
+		check = function(method) {
 			var v = null;
-			if(typeof _patch[method] == "function") {
-				v = _patch[method](_agent);
+			if(typeof patch[method] == "function") {
+				v = patch[method](agent);
 				switch (v){
 					case -1 : return false;
 					case 1 : return true;
@@ -13,11 +13,11 @@
 			}
 			return v;
 		},
-		_agent = (function() {
-			var osMatch = /(iPhone|iPad) OS ([\w.]+)/.exec(_ua) ||
-					/(Android) ([\w.]+)/.exec(_ua) ||
+		agent = (function() {
+			var osMatch = /(iPhone|iPad) OS ([\w.]+)/.exec(ua) ||
+					/(Android) ([\w.]+)/.exec(ua) ||
 					[],
-				browserMatch = /(Chrome|CriOS)[ \/]([\w.]+)/.exec(_ua) ||
+				browserMatch = /(Chrome|CriOS)[ \/]([\w.]+)/.exec(ua) ||
 					[];
 			if(osMatch.length >= 2) {
 				osMatch[1] = /iPhone|iPad/.test(osMatch[1]) ? "ios" : osMatch[1].toLowerCase() ;
@@ -25,7 +25,7 @@
 			}
 			if(browserMatch.length >= 2) {
 				if(/Chrome|CriOS/.test(browserMatch[1])) {
-					browserMatch[1] = /SAMSUNG/.test(_ua) ? "sbrowser" : "chrome";
+					browserMatch[1] = /SAMSUNG/.test(ua) ? "sbrowser" : "chrome";
 				}
 			}
 			return {
@@ -38,30 +38,30 @@
 
 	$.extend({
 		naver : {
-			defaults : _patch,
+			defaults : patch,
 			isHardwareAccelerable : function() {
-				var check = _check("isHardwareAccelerable");
+				var check = check("isHardwareAccelerable");
 				if(check != null) {
 					return check;
 				} else {
 					var result = false;
 					// chrome (less then 25) has a text blur bug.
 					// but samsung sbrowser fix it.
-					if(/chrome/.test(_agent.browser) && _agent.browserVersion < "25") {
+					if(/chrome/.test(agent.browser) && agent.browserVersion < "25") {
 						result = false;
-					} else if(/android/.test(_agent.os)) {
-						var ua = _ua.match(/\(.*\)/);
+					} else if(/android/.test(agent.os)) {
+						var ua = ua.match(/\(.*\)/);
 						if(ua instanceof Array && ua.length > 0){
 							ua=ua[0];
 						}
 						// android 4.1+ blacklist
 						// EK-GN120 : Galaxy Camera, SM-G386F : Galaxy Core LTE
-						result = (_agent.osVersion >= "4.1.0" && !/EK-GN120|SM-G386F/.test(ua)) ||
+						result = (agent.osVersion >= "4.1.0" && !/EK-GN120|SM-G386F/.test(ua)) ||
 							// SHW-M420 : Galaxy Nexus , SHW-M200 : NexusS , GT-S7562 : Galaxy S duos
-							(_agent.osVersion >= "4.0.3" &&
+							(agent.osVersion >= "4.0.3" &&
 								/SHW-|SHV-|GT-|SCH-|SGH-|SPH-|LG-F160|LG-F100|LG-F180|LG-F200|EK-|IM-A|LG-F240|LG-F260/.test(ua) &&
 								!/SHW-M420|SHW-M200|GT-S7562/.test(ua));
-					} else if(/ios/.test(_agent.os)) {
+					} else if(/ios/.test(agent.os)) {
 						result = true;
 					}
 					return result;
