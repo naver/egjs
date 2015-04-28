60a21c0511dbe6f518bd82f7ba6e9f3bba735c30
sculove <sculove@gmail.com>
2015-04-28 21:48:51 +0900

    replace gruntfile.js for auto build

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,9 +98,9 @@ module.exports = function(grunt) {
         }
     }
   });
-  
+
   grunt.registerTask("docBuild", ["copy:doc", "jsdoc"]);
   grunt.registerTask("test", ["jshint", "qunit"]);
-  grunt.registerTask("build", ["jshint", "concat", "uglify", "copy:lib", "jsdoc"]);
+  grunt.registerTask("build", ["concat", "uglify", "copy:lib", "docBuild"]);
   grunt.registerTask("default", ["test", "concat", "uglify", "copy:lib","copy:doc", "jsdoc"]);
 };
