56558f80abf50af7192b88e3004102c128462f3e
jongmoon <jongmoon@Naverui-MacBook-Pro-4.local>
2015-05-06 10:48:20 +0900

    Apply 'codepen' Feature to a flicking document.

--- src/flicking.js
+++ src/flicking.js
@@ -21,6 +21,8 @@
 	 * @param {Number} [options.duration=100] Duration time of panel change animation in milliseconds
 	 * @param {Function} [options.panelEffect=easing.easeInCubic] Function of the jQuery Easing Plugin
 	 * @param {Number} [options.defaultIndex=0] Default panel index to show in first time
+     * 
+	 * @codepen [{"id":"YPoqNg", "ko":"플리킹 기본 예제", "en":"Flicking default example"}]
 	 *
 	 * @see jQuery Easing Plugin {@link http://gsgd.co.uk/sandbox/jquery/easing}
 	 * @example
