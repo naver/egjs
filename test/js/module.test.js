b09ac1fb06dbb2389ba0dea91750665ceee5f8c4
Jae Sung Park <alberto.park@gmail.com>
2015-10-23 14:49:18 +0900

    test(module): Added test code for checking param ordinal numbers
    Updated TC checking error messages, and added TC for checking ordinal numbers.
    
    Ref g-48

--- test/js/module.test.js
+++ test/js/module.test.js
@@ -5,7 +5,7 @@ QUnit.module( "module", {
   }
 });
 
-QUnit.test( "A parameter is undefined.", function( assert ) {
+QUnit.test( "When a parameter is undefined.", function( assert ) {
   //Given
   var param = [window.something];
   //When
@@ -14,11 +14,11 @@ QUnit.test( "A parameter is undefined.", function( assert ) {
     function() {
       throw eg.module("test",param,function($) {});
     },
-    new Error("[egjs] The test in 1st argument is undefined.\n\rPlease check and try again.")
+    new Error("[egjs] The 1st argument of test is undefined.\n\rPlease check and try again.")
   );
 });
 
-QUnit.test( "The dependency library did not regist.", function( assert ) {
+QUnit.test( "The dependency library was not registered.", function( assert ) {
   //Given
   var param = ["notRegist"];
   //When
@@ -31,7 +31,7 @@ QUnit.test( "The dependency library did not regist.", function( assert ) {
   );
 });
 
-QUnit.test( "The dependency library registed but It does not use amd", function( assert ) {
+QUnit.test( "The dependency library was registered, but it does not use AMD", function( assert ) {
   //Given
   var param = ["jQuery"];
   //When
@@ -40,11 +40,11 @@ QUnit.test( "The dependency library registed but It does not use amd", function(
     function() {
       throw eg.module("notUseAmd",param,function($) {});
     },
-    new Error("[egjs] The notUseAmd in 1st argument is missing.\n\rDownload jQuery from [http://jquery.com/].")
+    new Error("[egjs] The 1st argument of notUseAmd is missing.\n\rDownload jQuery from [http://jquery.com/].")
   );
 });
 
-QUnit.test( "The dependency library registed but It did not define in amd.", function( assert ) {
+QUnit.test( "The dependency library was registered, but not defined in AMD.", function( assert ) {
   //Given
   var param = ["jQuery"];
   window.require = function(){
@@ -59,11 +59,11 @@ QUnit.test( "The dependency library registed but It did not define in amd.", fun
     function() {
       throw eg.module("notRegisteAmd",param,function($) {});
     },
-    new Error("[egjs] For AMD evnronment (like RequireJS), \"jQuery\" must be declared, which is required by notRegisteAmd."  )
+    new Error("[egjs] For AMD environment (like RequireJS), \"jQuery\" must be declared, which is required by notRegisteAmd."  )
   );
 });
 
-QUnit.test( "The dependency library registed and It defined in amd but It did not load.", function( assert ) {
+QUnit.test( "The dependency library was registered and defined in AMD, but not loaded.", function( assert ) {
   //Given
   var param = ["jQuery"];
   window.require = function(){
@@ -85,7 +85,7 @@ QUnit.test( "The dependency library registed and It defined in amd but It did no
   );
 });
 
-QUnit.test( "All parameters is object.", function( assert ) {
+QUnit.test( "All parameters are object.", function( assert ) {
   //Given
   var param = {};
   var param2 = {};
@@ -97,7 +97,7 @@ QUnit.test( "All parameters is object.", function( assert ) {
   });
 });
 
-QUnit.test( "A parameter is string and It did not load amd.", function( assert ) {
+QUnit.test( "When a parameter is string and not loaded as AMD.", function( assert ) {
   //Given
   window.jQuery = function(){};
   //When
@@ -107,7 +107,7 @@ QUnit.test( "A parameter is string and It did not load amd.", function( assert )
   });
 });
 
-QUnit.test( "A parameter is string and It loaded amd.", function( assert ) {
+QUnit.test( "When a parameter is string and not loaded as AMD.", function( assert ) {
   //Given
   var Hammer = {};
   window.require = function(){
@@ -123,5 +123,26 @@ QUnit.test( "A parameter is string and It loaded amd.", function( assert ) {
   //Then
   eg.module("like Hammer",["Hammer"],function(HM) {
     assert.strictEqual(Hammer, HM);
+  });
+});
+
+QUnit.test( "Check parameters ordinal numbers.", function( assert ) {
+  //Given
+  var param = [ 1, 1, 1, 1, 1 ];
+  //When
+
+  [ "1st", "2nd", "3rd", "4th", "5th" ].forEach(function(v,i) {
+    //Then
+    assert.throws(
+        function() {
+          var arr = param.concat();
+          arr[i] = window.something;
+
+          throw eg.module("test",arr,function($) {});
+        },
+        new Error("[egjs] The "+ v +" argument of test is undefined.\n\rPlease check and try again."),
+        "Ordinal number for "+ i +" is "+ v +"?"
+    );
+
   });
 });
