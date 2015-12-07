ab681dafb2c18e0a3af4af8bf1020110e83a7850
sculove <sculove@gmail.com>
2015-12-07 14:57:49 +0900

    chore(Gruntfile) : add dependency file
    ref e-178

---
+++ config/dependency.js
@@ -0,0 +1,30 @@
+module.exports = {
+	egCore : [
+		"src/module.js",
+		"src/eg.js",
+		"src/class.js",
+		"src/component.js"
+	],
+	egExtend : [
+		"src/customEvent/rotate.js",
+		"src/customEvent/scrollEnd.js",
+		"src/hook/animate.js",
+		"src/hook/css.js",
+		"src/plugin/apply.js",
+		"src/plugin/persist.js"
+	],
+	hammer : [
+		"bower_components/hammer.js/hammer.js"
+	],
+	outlayer : [
+		"bower_components/get-style-property/get-style-property.js",
+		"bower_components/get-size/get-size.js",
+		"bower_components/matches-selector/matches-selector.js",
+		"bower_components/eventEmitter/EventEmitter.js",
+		"bower_components/eventie/eventie.js",
+		"bower_components/doc-ready/doc-ready.js",
+		"bower_components/fizzy-ui-utils/utils.js",
+		"bower_components/outlayer/item.js",
+		"bower_components/outlayer/outlayer.js"
+	]
+};
