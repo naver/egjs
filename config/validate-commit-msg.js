b2b970968fb7747f637e7a3fe7330126d668005b
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-02-16 20:02:35 +0900

    style(commit-messge): Limit PR title
    When PR title is over 50 length Message title will truncate.
    So. PR title have to limit length.
    
    Reference) http://chris.beams.io/posts/git-commit/
    
    Ref #76

--- config/validate-commit-msg.js
+++ config/validate-commit-msg.js
@@ -14,7 +14,7 @@ var fs = require('fs');
 var util = require('util');
 
 
-var MAX_LENGTH = 100;
+var MAX_LENGTH = 50;
 var PATTERN = /^(?:fixup!\s*)?(\w*)(\(([\w\$\,\.\*/-]*)\))?\: (.*)$/;
 var IGNORED = /^WIP\:/;
 var TYPES = {
