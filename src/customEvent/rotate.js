ee9976d070a006821de4d15d2a0403c79f86f517
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-07-05 13:54:48 +0900

    fixed type 2 #277

--- src/customEvent/rotate.js
+++ src/customEvent/rotate.js
@@ -15,7 +15,7 @@ eg.module("rotate", ["jQuery", eg, window, document], function($, ns, global, do
 	/**
 	 * Add rotate event support in jQuery
 	 *
-	 * @ko 기기가 회전할 때 발생하는 orientationchange이벤트를 다양한 환경에서 사용할 수 있게 만든 jQuery의 커스텀 이벤트(rotate)다.
+	 * @ko 기기가 회전할 때 발생하는 orientationchange이벤트를 다양한 환경에서 사용할 수 있게 만든 jQuery의 커스텀 이벤트(rotate)이다.
 	 * @name jQuery#rotate
 	 * @event
 	 * @param {Event} e
