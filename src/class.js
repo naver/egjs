061002cb86ef4710262e31cdf4d682efeb451982
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2016-07-21 15:22:07 +0900

    docs(class): enhance document for eg.Class
    Ref #294

--- src/class.js
+++ src/class.js
@@ -10,12 +10,14 @@ eg.module("class", [eg], function(ns) {
 	 *
 	 * The Class object is used to implement object-oriented style programming
 	 * @group egjs
-	 * @ko Class는 어플리케이션을 객체지향 프로그래밍 방식으로 구현하는데 사용합니다.
+	 * @ko Class는 어플리케이션을 객체지향 프로그래밍 방식으로 구현하는데 사용한다.
 	 * @class
 	 * @name eg.Class
 	 *
 	 * @support {"ie": "7+", "ch" : "latest", "ff" : "latest",  "sf" : "latest", "edge" : "latest", "ios" : "7+", "an" : "2.1+ (except 3.x)"}
-	 * @param {Object} def Class definition of object literal type. <ko>리터럴 형태의 클래스 정의부</ko>
+	 * @param {Object} def Class definition of object literal type. <ko>클래스 정의부로 Object 의 <a href="https://en.wikipedia.org/wiki/Literal_(computer_programming)">리터럴 규칙</a>을 따른다. 단, "construct" 는 생성자 함수를 위해 예약된 키 값이다. </ko>
+	 * @param {Function} def.construct constructor of class. <ko>클래스 생성자 함수 (Optional)</ko>
+	 * 
 	 * @example
 	 	var Some = eg.Class({
 	 		//Class initialize
@@ -54,11 +56,12 @@ eg.module("class", [eg], function(ns) {
 	};
 	/**
 	 * Extends class
-	 * @ko extend는 Class를 상속할 때 사용합니다.
+	 * @ko extend는 Class를 상속할 때 사용한다.
 	 * @static
 	 * @method eg.Class.extend
 	 * @param {eg.Class} oSuperClass Super class. <ko>상속하려는 클래스</ko>
-	 * @param {Object} def Class definition of object literal type. <ko>리터럴 형태의 클래스 정의부</ko>
+	 * @param {Object} def Class definition of object literal type. <ko>클래스 정의부로 Object 의 <a href="https://en.wikipedia.org/wiki/Literal_(computer_programming)">리터럴 규칙</a>을 따른다. 단, "construct" 는 생성자 함수를 위해 예약된 키 값이다.</ko>
+	 * @param {Function} def.construct <ko>클래스 생성자 함수 (Optional)</ko>
 	 * @return {eg.Class} instance of new eg.Class <ko>새로 생성된 eg.Class 인스턴스</ko>
 	 * @example
 	 	var Some = eg.Class.extend(eg.Component,{
