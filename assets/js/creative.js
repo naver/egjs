4eb82d8ad97414d76cfb8286a0ad8db2b0df2883
Heejae Kim <heejae.kim@navercorp.com>
2016-10-19 18:09:33 +0900

    chore(all): make show case section

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -45,15 +45,114 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-
-     $("#flick").flicking({
-         duration : 300,
-         circular : true
-     });
-
-    $(window).resize(function() {
-        $("#flick").flicking("instance").resize();
+    // Initialize eg.Flicking Showcase
+    var padding = $("#flick").width() * 0.1;
+    $("#flick").flicking({
+      circular: true,
+      previewPadding: [ padding, padding ],
+      duration: 300,
+      defaultIndex: 3
     });
 
+     $(window).resize(function() {
+         // 이슈로 올리기
+         var padding = $("#flick").flicking("instance").$wrapper.width() * 0.1;
+         $("#flick").flicking("instance").options.previewPadding = [padding, padding];
+         $("#flick").flicking("instance").resize();
+     });
+
+     // Initialize eg.Visible Showcase
+     var visibleView = new eg.Visible('#visibleWrapper',{
+         targetClass : "card",
+         expandSize : -70
+     }).on("change", function (e) {
+         $(e.visible).addClass("visible");
+         $(e.invisible).removeClass("visible");
+     });
+
+     visibleView.check();
+
+     $('#visibleWrapper').scroll(function() {
+         visibleView.check();
+     });
+
+     // Initialize eg.InfiniteGrid Showcase
+     $("#infiniteGridWrapper").infiniteGrid({
+         itemSelector: ".item",
+         count: -1
+     });
+
+     // Initialize eg.MovableCoord Showcase
+     function gridItemHTML(items) {
+         return items.reduce(function(prv, cur) {
+             var html = '<div class="infinitegrid-item">' +
+     		'<div class="card">' +
+     			'<img class="' + cur.className + '" src="/assets/img/' + cur.className + '.svg" />' +
+     		'</div>' +
+             '</div>';
+             return prv + html;
+         }, '');
+     }
+    var griditemData = {
+    	getItems: function(groupNo) {
+    		groupNo *= 20;
+    		var items = [];
+    		for(var i=0; i<20; i++) {
+    			items.push(groupNo + i);
+    		}
+    		items = $.map(items, function(v) {
+                //console.log(v)
+    			return {
+				    offset: v,
+    				className: parseInt(10 * Math.random()) % 2 === 0 ? "logo_mono" : "type_white",
+    			};
+    		});
+    		return items;
+    	}
+     };
+     var $grid = $("#infiniteGridWrapper");
+
+     eg.InfiniteGrid.prototype._getScrollTop = function() {
+         return this.$el.parent().scrollTop();
+     };
+
+     var ig = new eg.InfiniteGrid("#infiniteGridWrapper", {
+         count : 60,
+         threshold : 500
+     }).on({
+         "append" : function(e) {
+             var gk = this.getGroupKeys();
+             var lastGk = gk[gk.length-1];
+             lastGk++;
+             console.log("appendL");
+
+             this.append(gridItemHTML(griditemData.getItems(lastGk)), lastGk);
+         },
+         "prepend" : function(e) {
+             var firstGk = this.getGroupKeys()[0];
+             firstGk--;
+             if(firstGk >= 0) {
+                 this.prepend(gridItemHTML(griditemData.getItems(firstGk)), firstGk);
+             }
+         },
+         "layoutComplete" : function(e) {
+             $grid.css("visibility", "visible");
+         }
+     });
+
+     ig.$view.off("scroll", ig._onScroll);
+     ig.$view.off("resize", ig._onResize);
+
+     ig.$view = $("#infiniteGridWrapper").parent();
+
+     ig.$view.on("scroll", ig._onScroll);
+     ig.$view.on("resize", ig._onResize);
+
+     $(window).resize(function() {
+         ig.layout();
+     });
+
+
+     ig.append(gridItemHTML(griditemData.getItems(0)), 0);
 
 })(jQuery); // End of use strict
