bfd437b78de034087da45526af9ee7a845d4e6cb
Jae Sung Park <alberto.park@gmail.com>
2017-03-21 18:07:31 +0900

    skip: Applied review
    change attribute name to be as constant

--- src/flicking.js
+++ src/flicking.js
@@ -83,6 +83,9 @@ eg.module("flicking", ["jQuery", eg, window, document, eg.MovableCoord], functio
 	var IS_ANDROID2 = ns.agent().os;
 	IS_ANDROID2 = IS_ANDROID2.name === "android" && /^2\./.test(IS_ANDROID2.version);
 
+	// data-height attribute's name for adaptiveHeight option
+	var DATA_HEIGHT = "data-height";
+
 	ns.Flicking = ns.Class.extend(ns.Component, {
 		_events: function() {
 			return EVENTS;
@@ -189,8 +192,7 @@ eg.module("flicking", ["jQuery", eg, window, document, eg.MovableCoord], functio
 					origCount: 0,		// total count of given original panels
 					changed: false,		// if panel changed
 					animating: false,	// current animating status boolean
-					minCount: padding[0] + padding[1] > 0 ? 5 : 3,  // minimum panel count
-					dataHeight: "data-height"   // data-height attribute's name for adaptiveHeight option
+					minCount: padding[0] + padding[1] > 0 ? 5 : 3  // minimum panel count
 				},
 				touch: {
 					holdPos: [0, 0],	// hold x,y coordinate
@@ -788,8 +790,6 @@ eg.module("flicking", ["jQuery", eg, window, document, eg.MovableCoord], functio
 			var $first;
 			var $children;
 			var height;
-			var dataName = "data-height";
-
 			var conf = this._conf;
 			var indexToMove = conf.indexToMove;
 
@@ -807,14 +807,14 @@ eg.module("flicking", ["jQuery", eg, window, document, eg.MovableCoord], functio
 				);
 
 			$first = $panel.find(":first");
-			height = $first.attr(dataName);
+			height = $first.attr(DATA_HEIGHT);
 
 			if (!height) {
 				$children = $panel.children();
 				height = ($children.length > 1 ? $panel.css("height", "auto") : $first)
 					.outerHeight(true);
 
-				$first.attr(dataName, height);
+				$first.attr(DATA_HEIGHT, height);
 			}
 
 			this.$wrapper.height(height);
@@ -1437,9 +1437,9 @@ eg.module("flicking", ["jQuery", eg, window, document, eg.MovableCoord], functio
 
 			// remove data-height attribute and re-evaluate panel's height
 			if (options.adaptiveHeight) {
-				var $panel = this.$container.find("[" + panel.dataHeight + "]");
+				var $panel = this.$container.find("[" + DATA_HEIGHT + "]");
 
-				$panel.size() && $panel.attr(panel.dataHeight, null) &&
+				$panel.size() && $panel.attr(DATA_HEIGHT, null) &&
 					this._setAdaptiveHeight();
 			}
 
