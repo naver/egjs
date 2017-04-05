0f2073f955c6ff35d430c101cfdcb6a414034b82
Jongmoon Yoon <jongmoon.yoon@gmail.com>
2017-04-05 15:55:31 +0900

    Support Opera Browser (#484)
    Ref #484

--- src/agent.js
+++ src/agent.js
@@ -19,6 +19,14 @@ eg.module("agent", [eg], function(ns) {
 			criteria: "PhantomJS",
 			identity: "PhantomJS"
 		}, {
+			criteria: /Opera Mini/,
+			identity: "Opera mini",
+			versionSearch: "Opera Mini"
+		}, {
+			criteria: /Opera|OPR/,
+			identity: "Opera",
+			versionSearch: "Opera|OPR"
+		}, {
 			criteria: /Edge/,
 			identity: "Edge",
 			versionSearch: "Edge"
